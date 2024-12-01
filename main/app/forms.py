5486e3fc801127fb5d1ea00370fca20ccc20c221
RaiyanRahmanReal <raiyanssrahman2001@gmail.com>
2024-12-02 00:27:48 +0600

    models.py  and forms.py done

--- main/app/forms.py
+++ main/app/forms.py
@@ -1,6 +1,14 @@
 
+from .models import Pet
 from django.contrib.auth.models import User
 from django import forms
+
+class SearchForm(forms.Form):
+    query = forms.CharField(label='Search')
+class PetForm(forms.ModelForm):
+    class Meta:
+        model = Pet
+        fields = ['name', 'price']
 
 class SignupForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
--- main/app/models.py
+++ main/app/models.py
@@ -2,6 +2,61 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
+class Pet(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_sold = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Pet(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    age = models.IntegerField()
+    location = models.CharField(max_length=100)
+    owner = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return self.breed
+
+    # new code:
+
+    from django.db import models
+
+
+class PetAdd(models.Model):
+    name = models.CharField(max_length=100)
+    species = models.CharField(max_length=100)
+    # pet_breed_code:
+    breed = models.CharField(max_length=100, default='unknown')
+    age = models.IntegerField()
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='pet_images/', null=True, blank=True)
+    image_url = models.URLField(null=True, blank=True)
+
+
+def image(self):
+    if self.image_upload:
+        return self.image_upload.url
+    elif self.image_url:
+        return self.image_url
+    else:
+        return None
+
+
+def save(self, *args, **kwargs):
+    if self.image_url:
+        # If image URL is provided, clear the uploaded image
+        self.image = None
+    super().save(*args, **kwargs)
+
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     # Additional fields if needed in the future, e.g., profile picture, bio, etc.
