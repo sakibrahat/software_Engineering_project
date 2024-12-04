09c018926da557ac05618adf1818b23f4a772035
Rafid Al Razi <rafidalrazi@gmail.com>
2024-12-04 21:54:03 +0600

    1st commit.

--- registration/app1/views.py
+++ registration/app1/views.py
@@ -1,9 +1,9 @@
-
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
 
 @login_required(login_url='login')
 def homepage(request):
